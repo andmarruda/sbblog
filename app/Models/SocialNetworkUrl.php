cd39bb20ffe1f43421b8aaa00556b911f7dbeb7f
Anderson ARruda <andmarruda@gmail.com>
2022-05-25 22:56:18 -0300

    preparing to show social network at public admin

--- app/Models/SocialNetworkUrl.php
+++ app/Models/SocialNetworkUrl.php
@@ -9,4 +9,16 @@ class SocialNetworkUrl extends Model
 {
     use HasFactory;
     protected $fillable = ['general_id', 'social_network_id', 'url'];
+
+    /**
+     * Get instance of SocialNetwork that is parent from this model
+     * @version         1.0.0
+     * @author          Anderson Arruda < andmarruda@gmail.com >
+     * @param           
+     * @return          Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function socialNetwork() : \Illuminate\Database\Eloquent\Relations\Belongsto
+    {
+        return $this->belongsTo(SocialNetwork::class);
+    }
 }
