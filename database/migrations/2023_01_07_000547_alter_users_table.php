23747b01a21cf1866891698eb8962eb3323e2d59
Anderson ARruda <andmarruda@gmail.com>
2023-01-07 00:41:06 -0300

    improving user pattern

--- database/migrations/2023_01_07_000547_alter_users_table.php
+++ database/migrations/2023_01_07_000547_alter_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -13,7 +14,19 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        //remove active and add softdelete
+        Schema::table('users', function(Blueprint $table){
+            $table->softDeletesTz();
+        });
+
+        foreach(User::all() as $user){
+            if(!$user->active)
+                $user->delete();
+        }
+
+        Schema::table('users', function(Blueprint $table){
+            $table->dropColumn('active');
+        });
     }
 
     /**
@@ -23,6 +36,20 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        //add active and drop softdelete
+        Schema::table('users', function(Blueprint $table){
+            $table->boolean('active')->default('true');
+        });
+
+        foreach(User::withTrashed()->all() as $user){
+            if(!is_null($user->deleted_at)){
+                $user->active = false;
+                $user->save();
+            }
+        }
+
+        Schema::table('users', function(Blueprint $table){
+            $table->dropSoftDeletesTz();
+        });
     }
 };
