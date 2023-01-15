f0fa89463294464bcdb8eaf0106bb6d5df4515d9
Anderson ARruda <andmarruda@gmail.com>
2023-01-15 13:19:52 -0300

    improved admin layout, footer always in footer, success and error message now are position static on the bottom

--- resources/views/utils/alertError.blade.php
+++ resources/views/utils/alertError.blade.php
@@ -1,9 +1,14 @@
 @if($errors->any())
-<div class="alert alert-danger mb-3" role="alert">
-    <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-    </ul>
+<div class="fixed-bottom" id="alert-error">
+    <div class="container-md">
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    </div>
 </div>
 @endif
--- resources/views/utils/alertSuccess.blade.php
+++ resources/views/utils/alertSuccess.blade.php
@@ -1,4 +1,8 @@
-<div class="alert alert-success alert-dismissible fade show fixed-bottom">
-    {{$message}}
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+<div class="fixed-bottom" id="alert-success">
+    <div class="container-md">
+        <div class="alert alert-success alert-dismissible fade show">
+            {{$message}}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    </div>
 </div>
