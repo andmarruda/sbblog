91111fae5dc52c583d7c1aa4778e2505ec32663d
Anderson <andmarruda@gmail.com>
2023-10-09 15:27:23 -0300

    fixed bug on seeder inside User table creation

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
         });
 
         Artisan::call('db:seed', [
-            '--class' => 'UsersSeeder',
+            '--class' => 'UserSeeder',
             '--force' => true
         ]);
     }
